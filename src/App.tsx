32c79062aa281aa5f02a9228606e7bbd947ee1da
Javier Cabrera <javier.cabrera@wundermanthompson.com>
2023-02-07 00:54:18 -0300

    Add Yarn. Add some packages and remove unused files.

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./assets/scss/styles.scss";
+import React from "react";
+
+
+const App = () => {
+
+  return (
+    <div className="App">
+    </div>
+  );
+}
+
+export default App;
---
+++ src/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import  './index.scss';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <p>Hola</p>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
